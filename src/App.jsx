51802ab15a73372ddff79f961cc0177d984bb70f
Israel Kenneth <israel.kenneth2011@gmal.com>
2023-08-30 01:19:50 +0200

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+import { CustomerReviews, PopularProducts, Subscribe, Footer, Services, SpecialOffer, SuperQuality, Hero } from "./sections";
+import { Navbar } from "./components";
+
+
 const App = () => (
   <main className="relative">
-    Nav
+    <Navbar />
     <section className="xl:padding-1 wide:padding-r padding-b ">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
+    </section>
+        <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+        <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+        <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
     </section>
   
   </main>
---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import CustomerReviews from './CustomerReviews'
+import PopularProducts from './PopularProducts'
+import Subscribe from './Subscribe'
+import Nav from './Nav'
+import Footer from './Footer'
+import Services from './Services'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import Hero from './Hero'
+
+export {CustomerReviews, PopularProducts, Subscribe, Nav, Footer, Services, SpecialOffer, SuperQuality, Hero}
